6c8844ebc1d32ea663bf089796a6e6bacbae48da
fernando villalobos <fvd_24_93@hotmail.com>
2022-08-16 00:49:28 -0500

    controller for the contact route to manage the data to send created

---
+++ controllers/contact.js
@@ -0,0 +1,46 @@
+const nodemailer = require("nodemailer");
+
+const contactEmail = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.GMAIL_USER,
+        pass: process.env.GMAIL_PASS
+    },
+});
+
+contactEmail.verify((error) => {
+    if(error) {
+        console.log(error);
+    } else {
+        console.log("Ready to Send");
+    }
+});
+
+const postMail = (req, res) => {
+    const {name ,email, message} = req.body
+
+    res.send("Contact");
+    const mail = {
+        from: name,
+        to: process.env.GMAIL_USER,
+        subject: `Message from ${name}`,
+        html: `
+        <h2>Name:</h2>
+        <p>${name}</p>
+        <h2>Email:</h2>
+        <p>${email}</p>
+        <h2>Message:</h2>
+        <p>${message}</p>
+        `
+    };
+    contactEmail.sendMail(mail, (err) => {
+        if(err) {
+            res.json({status: "Error"});
+        } else {
+            res.json({ status: "Message Sent"});
+        }
+    });
+}
+
+module.exports = postMail;
+
